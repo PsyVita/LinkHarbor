252b3fb48ad39d2bb809936f2eb61327d8e0b157
skyfish154 <psyvita.iv@gmail.com>
2024-10-07 12:09:50 +0700

    Refactor code to remove unused files and update project properties

--- config.js
+++
@@ -1,2 +0,0 @@
-//const MY_API_KEY = 'dd088ba9c0mshe221a2f6de24c80p1d22c2jsna74373564521';
-//export { MY_API_KEY };
--- test/README.txt
+++
@@ -1,4 +0,0 @@
-
-Google Sheet ID:
-1B9UZAbmIlg1r0A60xlsBc23hU5uVed8sWiJGvR6IDnw
-
